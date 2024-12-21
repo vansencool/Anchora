e33ea48be25b278482f8e43bd919b3f85685a610
vansencool <vansencool@gmail.com>
2024-12-21 19:13:18 +0530

    Base start.

---
+++ src/main/java/dev/vansen/anchora/Anchora.java
@@ -0,0 +1,17 @@
+package dev.vansen.anchora;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Anchora extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
